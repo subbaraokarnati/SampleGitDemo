144161023752ed1a983155531a45d76f52a48f8f
KA376091 <KA376091@D-113074638.wipro.com>
2017-12-11 14:16:54 +0530

    initiate check in for Git Demo

---
+++ GitDemo/src/com/sample/Sample.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class Sample {
+
+	public void display(){
+		System.out.println("First sample git application!!");
+		System.out.println("Welcome to the git world!!");
+	}
+}
